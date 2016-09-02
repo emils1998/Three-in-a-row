5440338e0a6b8a041845cc56e2e7e6c2ab8d1037
scem14 <te14.emil.schmidt@umea.nti.se>
2016-09-02 11:23:42 +0200

    klar

---
+++ src/Test.java
@@ -0,0 +1,36 @@
+import java.util.scanner
+/**
+ * Created by scem14 on 2016-09-02.
+ */
+public class Test {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Skriv ett nummer");
+        int n1 = input.nextInt();
+        System.out.println("Skriv ett nummer");
+        int n2 = input.nextInt();
+        System.out.println("Skriv ett nummer");
+        int n3 = input.nextInt();
+        System.out.println("Skriv ett nummer");
+        int n4 = input.nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
