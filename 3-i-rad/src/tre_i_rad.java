0bb7e3ea6ac877debd292388f93e428cf5ef3d7b
scem14 <te14.emil.schmidt@umea.nti.se>
2016-09-09 09:43:00 +0200

    1st

---
+++ 3-i-rad/src/tre_i_rad.java
@@ -0,0 +1,10 @@
+/**
+ * Created by scem14 on 2016-09-09.
+ */
+public class tre_i_rad {
+    public static void main(String[] arg){
+
+        String P1, P2;
+        String
+    }
+}
--- src/Tre_I_Rad.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by scem14 on 2016-09-09.
- */
-public class Tre_I_Rad {
-}
