6193a5942dc0c9f3497bab948b6d43abd67d27a0
scem14 <te14.emil.schmidt@umea.nti.se>
2016-09-09 10:15:28 +0200

    Början - Lagt till "rutorna" till planen

--- 3-i-rad/src/tre_i_rad.java
+++ 3-i-rad/src/tre_i_rad.java
@@ -2,9 +2,26 @@
  * Created by scem14 on 2016-09-09.
  */
 public class tre_i_rad {
-    public static void main(String[] arg){
+    char x1;
+    char x2;
+    char x3;
+    char z4;
+    char z5;
+    char z6;
+    char w7;
+    char w8;
+    char w9;
 
-        String P1, P2;
-        String
+    public static void main(String[] arg) {
+        char x1 = '-';
+        char x2 = '-';
+        char x3 = '-';
+        char z4 = '-';
+        char z5 = '-';
+        char z6 = '-';
+        char w7 = '-';
+        char w8 = '-';
+        char w9 = '-';
+        }
     }
 }
